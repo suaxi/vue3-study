29fb7cd66d3fe15882d539852908cc10f1e837bf
wanghao <281463547@qq.com>
2022-07-21 22:36:38 +0800

    登录页面及校验规则

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,11 +1,57 @@
 <template>
-
+  <el-form ref="ruleFormRef" :model="ruleForm" :rules="rules" class="demo-ruleForm">
+    <el-form-item prop="username">
+      <el-input v-model="ruleForm.username" type="text" autocomplete="off"/>
+    </el-form-item>
+    <el-form-item prop="password">
+      <el-input v-model="ruleForm.password" type="password" autocomplete="off"/>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="login">登录</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
-<script>
-export default {
-  name: "login"
+<script lang="ts" setup>
+import {reactive, ref, toRefs} from "vue";
+
+const state = reactive({
+  ruleForm: {
+    username: '',
+    password: ''
+  }
+})
+//获取el-form组件对象
+let ruleFormRef = ref()
+
+const validatePwd = (rule: unknown, value: string | undefined, callback: (msg?: string) => void) => {
+  if (!value) {
+    callback('密码不能为空')
+  } else {
+    callback()
+  }
 }
+
+//校验规则
+const rules = reactive({
+  username: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+  ],
+  password: [
+    { validator: validatePwd, trigger: 'blur' }
+  ]
+})
+
+let {ruleForm} = toRefs(state)
+
+const login = () => {
+  ruleFormRef.value.validate().then(() => {
+    console.log('then')
+  }).catch(() => {
+    console.log('catch')
+  })
+}
+
 </script>
 
 <style scoped>
